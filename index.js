0f9fbfe94910ee0b6a203d495a71947546c7a71e
dexter <pogeraf2001@gamil.com>
2017-06-06 17:57:26 +0300

    ``` chat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var io = require('socket.io')(http);
-
+ 
 
 app.use( bodyParser.json() );
 
